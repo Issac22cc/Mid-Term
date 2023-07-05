d633580a7a8e6e86d8b815c0b7170f552e3bf191
Issac Leung <lleung49@my.centennialcollege.ca>
2023-07-05 12:21:59 -0600

    first commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+url:"mongodb+srv://Issac23cc:Issac0720_-@cluster0.ucdzggw.mongodb.net/SportStore?retryWrites=true&w=majority"
+
+};
